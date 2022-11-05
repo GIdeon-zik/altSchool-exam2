32acbe8cc2307dc0307c8fcf5e1e990b9518df19
Ejiogu Gideon <104659741+GIdeon-zik@users.noreply.github.com>
2022-11-05 22:14:18 +0100

    Create Home.js

---
+++ src/Home.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import createReducer from './components/ReactCounter';
+import { useReducer } from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+const initialState = { count: 0 };
+
+const reducer = createReducer(initialState, {
+  reset: () => initialState,
+  increment: state => ({ count: state.count + 1 }),
+  decrement: state => ({ count: state.count - 1 })
+});
+
+function Home ({ initialCount }) {
+  const [state, dispatch] = useReducer(reducer, { count: initialCount });
+  return (
+    <>
+      Count: {state.count}
+      <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
+      <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+      <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+    </>
+  );
+}
+export default Home;
