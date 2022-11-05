a739bdb46a192856d461143d74e986fab5fa7b0d
Ejiogu Gideon <104659741+GIdeon-zik@users.noreply.github.com>
2022-11-05 22:12:26 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import React from 'react'
-import createReducer from './components/ReactCounter';
-import { useReducer } from 'react';
-import ReactDOM from 'react-dom';
-import './style.css';
+import React from "react";
+import { Route, Switch, BrowserRouter }
+		from 'react-router-dom'
+import Home from '.Home';
+import PageNotFound from './404Page';
 
-const initialState = { count: 0 };
-
-const reducer = createReducer(initialState, {
-  reset: () => initialState,
-  increment: state => ({ count: state.count + 1 }),
-  decrement: state => ({ count: state.count - 1 })
-});
-
-function App({ initialCount }) {
-  const [state, dispatch] = useReducer(reducer, { count: initialCount });
-  return (
-    <>
-      Count: {state.count}
-      <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
-      <button onClick={() => dispatch({ type: 'increment' })}>+</button>
-      <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
-    </>
-  );
+function App() {
+return (
+	<BrowserRouter>
+	<Switch>
+		<Route exact path='/' component={Home} />
+		<Route path="*" component={PageNotFound} />
+	</Switch>
+	</BrowserRouter>
+);
 }
 
-
-export default App
+export default App;
