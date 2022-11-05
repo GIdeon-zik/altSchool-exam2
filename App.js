ae7f359bd4b1423226f9c08a45db3a5c80ee8fe8
Ejiogu Gideon <104659741+GIdeon-zik@users.noreply.github.com>
2022-11-05 18:31:33 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import React from 'react'
-import ReactCounter from './components/ReactCounter'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-function App() {
+import ReactCounter from './components/createReducer';
+import React, { useReducer } from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+
+const initialState = { count: 0 };
+
+const reducer = createReducer(initialState, {
+  reset: () => initialState,
+  increment: state => ({ count: state.count + 1 }),
+  decrement: state => ({ count: state.count - 1 })
+});
+
+function App({ initialCount }) {
+  const [state, dispatch] = useReducer(reducer, { count: initialCount });
   return (
-    <div className="container mt-3">
-      <ReactCounter />
-    </div>
-  )
+    <>
+      Count: {state.count}
+      <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
+      <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+      <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+    </>
+  );
 }
+
+
 export default App
